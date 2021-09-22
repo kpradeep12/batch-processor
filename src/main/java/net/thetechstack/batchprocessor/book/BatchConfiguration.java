27f2cfd1a67086fc0a7304e15e461e8fa87e985e
Pradeep <kpradeep12@users.noreply.github.com>
2021-09-22 19:27:49 -0400

    switch to mysql

--- src/main/java/net/thetechstack/batchprocessor/book/BatchConfiguration.java
+++ src/main/java/net/thetechstack/batchprocessor/book/BatchConfiguration.java
@@ -1,5 +1,9 @@
 package net.thetechstack.batchprocessor.book;
 
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -8,11 +12,10 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.data.builder.MongoItemWriterBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.LineMapper;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,21 +31,26 @@ public class BatchConfiguration {
     @Autowired public StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public FlatFileItemReader<Book> reader() {
-        FlatFileItemReader<Book> itemReader = new FlatFileItemReader<>();
+    public FlatFileItemReader<Map<String, Object>> reader() {
+        FlatFileItemReader<Map<String, Object>> itemReader = new FlatFileItemReader<>();
         itemReader.setLinesToSkip(0);
         itemReader.setName("item-reader");
-        itemReader.setResource(new FileSystemResource("/Users/pradeep/OneDrive/code/books.csv"));
-        DefaultLineMapper lineMapper = new DefaultLineMapper<>();
-        lineMapper.setLineTokenizer(new DelimitedLineTokenizer(","));
-        itemReader.setLineMapper(lineMapper);
+        itemReader.setResource(new FileSystemResource("C:/Users/PRADEEP/OneDrive/code/books_test.csv"));
+        DefaultLineMapper<Map<String, Object>> mapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(",");
+        tokenizer.setNames(new String[] {"bookId","goodReadsBookId"});
+        //tokenizer.setNames(new String[] {"bookId","goodReadsBookId", "bestBookId", "workId", "booksCount", "isbn", "authors", "originalPublicationYear"
+        //, "originalTitle", "title", "languageCode", "averageRating", "ratingsCount", "workRatingsCount", "workTextReviewCount", "ratings1", "ratings2", "ratings3", "ratings4", "ratings5", "imageUrl", "smallImageUrl"});
+        mapper.setLineTokenizer(tokenizer);
+        mapper.setFieldSetMapper(new CustomFieldSetMapper());
+        itemReader.setLineMapper(mapper);
         return itemReader;
         /*return new FlatFileItemReaderBuilder<Book>()
             .name("item-reader")
-            .resource(new FileSystemResource("/Users/pradeep/OneDrive/code/books.csv"))
+            .resource(new FileSystemResource("C:/Users/PRADEEP/OneDrive/code/books.csv"))
             .delimited()
             
-            .names(new String[] {"bookId","goodReadsBookId", "bestBookId", "workId", "booksCount", "isbn", "isbn13", "authors", "originalPublicationYear"
+            .names(new String[] {"bookId","goodReadsBookId", "bestBookId", "workId", "booksCount", "isbn", "authors", "originalPublicationYear"
             , "originalTitle", "title", "languageCode", "averageRating", "ratingsCount", "workRatingsCount", "workTextReviewCount", "ratings1", "ratings2", "ratings3", "ratings4", "ratings5", "imageUrl", "smallImageUrl"})
             .fieldSetMapper(new BeanWrapperFieldSetMapper<Book>() {{
                 setTargetType(Book.class);
@@ -51,14 +59,23 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public MongoItemWriter<Book> writer(MongoTemplate mongoTemplate) {
-        return new MongoItemWriterBuilder<Book>().template(mongoTemplate).collection("books")
+    public MongoItemWriter<Map<String, Object>> writer(MongoTemplate mongoTemplate) {
+        return new MongoItemWriterBuilder<Map<String, Object>>().template(mongoTemplate).collection("books")
                 .build();
     }
 
     @Bean
-    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
-    return jobBuilderFactory.get("importUserJob")
+    public JdbcBatchItemWriter<Map<String, Object>> writer(DataSource dataSource) {
+    return new JdbcBatchItemWriterBuilder<Map<String, Object>>()
+        .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+        .sql("INSERT INTO people (bookId, goodReadsBookId) VALUES (:bookId, :goodReadsBookId)")
+        .dataSource(dataSource)
+        .build();
+    }
+
+    @Bean
+    public Job dataProcessorJob(JobCompletionNotificationListener listener, Step step1) {
+    return jobBuilderFactory.get("dataProcessorJob")
         .incrementer(new RunIdIncrementer())
         .listener(listener)
         .flow(step1)
@@ -67,10 +84,10 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Step step1(FlatFileItemReader<Book> reader, MongoItemWriter<Book> writer) {
+    public Step step1(FlatFileItemReader<Map<String, Object>> reader, JdbcBatchItemWriter<Map<String, Object>> writer) {
     return stepBuilderFactory.get("step1")
-        .<Book, Book> chunk(10)
-        .reader(reader())
+        .<Map<String, Object>, Map<String, Object>> chunk(10)
+        .reader(reader)
         .writer(writer)
         .build();
     }
--- src/test/java/net/thetechstack/batchprocessor/BatchProcessorApplicationTests.java
+++ src/test/java/net/thetechstack/batchprocessor/BatchProcessorApplicationTests.java
@@ -1,13 +1,33 @@
 package net.thetechstack.batchprocessor;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import net.thetechstack.batchprocessor.book.BatchConfiguration;
+
+@SpringBatchTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes=BatchConfiguration.class)
 class BatchProcessorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    
+    /*@Test
+    public void testJob() throws Exception {
+        
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+
+        Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+    }*/
 
 }
